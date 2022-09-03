6e944831b394aa4b31efcd89eb88d3b196b68d4e
HLL <snow@inspiralcreative.com>
2022-09-03 14:10:45 +0630

    day 5

---
+++ sound-board/script.js
@@ -0,0 +1,23 @@
+const sounds = ['bigstorm','fire','heavyrainloop','oceanwave','sea']
+
+sounds.forEach(sound =>{
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerHTML= sound
+
+    btn.addEventListener('click',()=>{
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs(){
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+        song.pause()
+        song.currentTime=0
+    })
+}
